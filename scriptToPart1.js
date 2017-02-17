fbd898741a93e0d05031a8eab6d31198cda78241
GreatDrummer2 <phillippochta@yahoo.com>
2017-02-17 18:32:33 +0200

    comments

--- scriptToPart1.js
+++ scriptToPart1.js
@@ -1,17 +1,17 @@
 ;(function(){
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET','https://dl.dropboxusercontent.com/u/53408179/phones.json',true);
-  xhr.send();
+  var xhr = new XMLHttpRequest(); //создаем запрос
+  xhr.open('GET','https://dl.dropboxusercontent.com/u/53408179/phones.json',true); // настраиваем
+  xhr.send(); //посылаем
   xhr.onreadystatechange = function(){
-    if(xhr.readyState != 4) return;
+    if(xhr.readyState != 4) return; // если еще ничего не получили-ничего не надо возвращать
     if(xhr.status != 200){
-      alert(xhr.status +':'+xhr.statusText);
+      alert(xhr.status +':'+xhr.statusText); // если есть ошибки выводим айди ошибки
     }
     else{
-     array = JSON.parse(xhr.responseText);
-     var list= document.createElement('ul');
+     array = JSON.parse(xhr.responseText); // JSON to JS
+     var list= document.createElement('ul'); // создаем список
      document.body.appendChild(list);
-     for(var i=0; i < array.length;i++){
+     for(var i=0; i < array.length;i++){ // добавляем элементы списка
        var li = document.createElement('li');
        li.innerHTML = array[i].name;
        list.appendChild(li);
